70dab2b37ad5a6e38053f24005396aa86f260319
Ivan Soriano <ivan.soriano@live.com.ar>
2021-12-19 20:07:17 -0300

    Add course filters

--- src/components/Courses/component.tsx
+++ src/components/Courses/component.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './styles.css';
 import { Course } from './Course';
+import { Button } from "@mui/material";
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
-
-export const CoursesList = ({ courses }: any) => {
+export const CoursesList = ({ courses, subFilter, typeFilter, onSubFilterChange, onTypeFilterChange  }: any) => {
   return (
     <div className='CoursesList'>
       <div className='Description'>
@@ -14,6 +16,34 @@ export const CoursesList = ({ courses }: any) => {
       {(courses.length === 0) ? <></> : 
       courses.map((course: any) => 
         <Course course={course}/>)}
+
+      <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={subFilter}
+          label="Subscription Type"
+          onChange={(e) => onSubFilterChange(e.target.value)}
+        >
+          <MenuItem value={"None"}>None</MenuItem>
+          <MenuItem value={"Free"}>Free</MenuItem>
+          <MenuItem value={"Silver"}>Silver</MenuItem>
+          <MenuItem value={"Gold"}>Gold</MenuItem>
+          <MenuItem value={"Platinum"}>Platinum</MenuItem>
+      </Select>
+
+      <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={typeFilter}
+          label="Course Type"
+          onChange={(e) => onTypeFilterChange(e.target.value)}
+        >
+          <MenuItem value={"None"}>None</MenuItem>
+          <MenuItem value={"Programming"}>Programming</MenuItem>
+          <MenuItem value={"Art"}>Art</MenuItem>
+          <MenuItem value={"Music"}>Music</MenuItem>
+          <MenuItem value={"Math"}>Math</MenuItem>
+      </Select>
     </div>
   );
 }
--- src/components/Courses/container.tsx
+++ src/components/Courses/container.tsx
@@ -4,10 +4,28 @@ import { CoursesList } from "./component";
 
 export const CoursesListContainer = () => {
   const { courses } = useGetCourses();
+  let [subFilter, setSubFilter] = React.useState("None");//Filtro del tipo de suscripcion del curso
+  let [typeFilter, setTypeFilter] = React.useState("None");//Filtro del tipo de curso
+
+  const handleApplyFilter = (_courses:any) => {
+    return _courses.filter((course:any) => (subFilter === "None" || course.subscription_type === subFilter) && (typeFilter === "None" || course.course_type === typeFilter));//No esta rerenderizando los courses
+  }
+
+  const handleSubFilterChange = (value: any) => {
+    setSubFilter(value);
+  }
+
+  const handleTypeFilterChange = (value: any) => {
+    setTypeFilter(value);
+  }
 
   return (
     <CoursesList
-      courses={courses}
+      courses={handleApplyFilter(courses)}
+      onSubFilterChange={handleSubFilterChange}
+      onTypeFilterChange={handleTypeFilterChange}
+      subFilter={subFilter}
+      typeFilter={typeFilter}
     />
   );
 }
